14406fba6d7f3d0c9c8a00fc17004f580c8271af
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-08-08 22:10:53 +0630

    test:slider

--- src/slider/entities/slider.entity.ts
+++ src/slider/entities/slider.entity.ts
@@ -1,46 +1,41 @@
 import { Exclude, Expose, Transform } from 'class-transformer';
 import { MediaEntity } from 'src/media';
-import { ProductEntity } from 'src/products';
-import { createEntity } from 'src/shared/utils/createEntity';
 import { UserEntity } from 'src/users/entities/user.entity';
 
 export class SliderEntity {
   id: number;
 
   @Expose()
-  @Transform(({ value }) => (value ? new MediaEntity(value).image() : null))
-  place1Desktop: MediaEntity;
+  @Transform(({ obj, key }) => obj[key] ? new MediaEntity(obj[key]).image() : null)
+  place1Desktop: string;
 
   @Expose()
-  @Transform(({ value }) => (value ? new MediaEntity(value).image() : null))
-  place1Mobile: MediaEntity;
+  @Transform(({ obj, key }) => obj[key] ? new MediaEntity(obj[key]).image() : null)
+  place1Mobile: string;
 
   @Expose()
-  @Transform(({ value }) => (value ? new MediaEntity(value).image() : null))
-  place2Desktop: MediaEntity;
+  @Transform(({ obj, key }) => obj[key] ? new MediaEntity(obj[key]).image() : null)
+  place2Desktop: string;
 
   @Expose()
-  @Transform(({ value }) => (value ? new MediaEntity(value).image() : null))
-  place2Mobile: MediaEntity;
+  @Transform(({ obj, key }) => obj[key] ? new MediaEntity(obj[key]).image() : null)
+  place2Mobile: string;
 
   @Expose()
-  @Transform(({ value }) => (value ? new MediaEntity(value).image() : null))
-  place3Desktop: MediaEntity;
+  @Transform(({ obj, key }) => obj[key] ? new MediaEntity(obj[key]).image() : null)
+  place3Desktop: string;
 
   @Expose()
-  @Transform(({ value }) => (value ? new MediaEntity(value).image() : null))
-  place3Mobile: MediaEntity;
+  @Transform(({ obj, key }) => obj[key] ? new MediaEntity(obj[key]).image() : null)
+  place3Mobile: string;
 
   @Expose()
-  @Transform(({ value }) => (value ? new MediaEntity(value).image() : null))
-  place4Desktop: MediaEntity;
+  @Transform(({ obj, key }) => obj[key] ? new MediaEntity(obj[key]).image() : null)
+  place4Desktop: string;
 
   @Expose()
-  @Transform(({ value }) => (value ? new MediaEntity(value).image() : null))
-  place4Mobile: MediaEntity;
-
-  photosForMobile: { img1: string; img2: string; img3: string; img4: string };
-  photosForDesktop: { img1: string; img2: string; img3: string; img4: string };
+  @Transform(({ obj, key }) => obj[key] ? new MediaEntity(obj[key]).image() : null)
+  place4Mobile: string;
 
   @Exclude()
   createAt: Date;
@@ -50,48 +45,48 @@ export class SliderEntity {
 
   @Exclude()
   createdByUserId: number;
+
   @Exclude()
   updatedByUserId: number;
 
   createdByUser?: UserEntity;
   updatedByUser?: UserEntity;
 
-  constructor(slider: any,data: Partial<SliderEntity> = {}) {
+  constructor(slider: any, data: Partial<SliderEntity> = {}) {
     Object.assign(this, data);
-
 
     const mediaKeys = [
       {
-        desktopImage:  'place1Desktop',
-        mobileImage : 'place1Mobile',
-        sort : 1,
+        desktopImage: 'place1Desktop',
+        mobileImage: 'place1Mobile',
+        sort: 1,
       },
       {
-        desktopImage:'place2Desktop',
+        desktopImage: 'place2Desktop',
         mobileImage: 'place2Mobile',
-        sort:2
+        sort: 2,
       },
       {
-        desktopImage : 'place3Desktop',
+        desktopImage: 'place3Desktop',
         mobileImage: 'place3Mobile',
         sort: 3,
       },
       {
         desktopImage: 'place4Desktop',
         mobileImage: 'place4Mobile',
-        sort: 4
-      }
-
+        sort: 4,
+      },
     ];
 
     mediaKeys.forEach(key => {
       if (slider[key.desktopImage]) {
-        this[key.desktopImage] = new MediaEntity(slider[key.desktopImage]);
+        this[key.desktopImage] = slider[key.desktopImage];
       }
 
       if (slider[key.mobileImage]) {
-        this[key.mobileImage] = new MediaEntity(slider[key.mobileImage]);
+        this[key.mobileImage] = slider[key.mobileImage];
       }
     });
   }
 }
+
